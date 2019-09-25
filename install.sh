310efcffc16a489b634ff043aac6b9b9cf1320d1
Luis Eduardo Ferro Diez <luisedof10@gmail.com>
2019-09-25 15:57:24 -0500

    Fix git submodule configuration and quickfix the tmux.conf file

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ DOTBOT_BIN="bin/dotbot"
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 cd "${BASEDIR}"
-git -C "${DOTBOT_DIR}" submodule sync --quiet --recursive
 git submodule update --init --recursive
+git -C "${DOTBOT_DIR}" submodule sync --quiet --recursive
 
 "${BASEDIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${BASEDIR}" -c "${CONFIG}" "${@}"
